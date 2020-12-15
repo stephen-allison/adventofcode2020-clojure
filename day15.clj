2857bc4e02413095bdd190ed3ef72b284deb2bf8
Stephen Allison <git@steve.allison.clara.co.uk>
2020-12-15 23:04:36 +0000

    use transient - incrrases speed 2x

--- day15.clj
+++ day15.clj
@@ -3,6 +3,9 @@
 (require '[clojure.string :as str])
 (require '[clojure.set])
 
+; lazy evaluation using iterate
+; slower than anticipated
+
 (defn initial-index [input]
   (zipmap input (range 1 (count input))))
 
@@ -10,15 +13,31 @@
   [[last-spoken n index]]
   (let [prev (get index last-spoken)
         next (if (nil? prev) 0 (- n prev))]
-    [next (inc n) (assoc index last-spoken n)]))
+    [next (inc n) (assoc! index last-spoken n)]))
 
 (defn game-turns
   [input]
-  (iterate game-turn [(last input) (count input) (initial-index input)]))
+  (iterate game-turn [(last input) (count input) (transient (initial-index input))]))
 
 (defn play-day15
   [input turns]
   (first (first (drop (- turns (count input)) (game-turns input)))))
+
+; simpler but quicker recursive approach
+
+(defn day-15
+  [input turns]
+  (loop [n (count input)
+         index (transient (initial-index input))
+         last-spoken (last input)]
+    (if (< n turns)
+      (let [prev (get index last-spoken)
+            next (if (nil? prev) 0 (- n prev))]
+        (recur (inc n) (assoc! index last-spoken n) next))
+      last-spoken)))
+
+(defn day15-recursive []
+  (day-15 [1 12 0 20 8 16] 30000000))
 
 (defn -main
   [& args]
